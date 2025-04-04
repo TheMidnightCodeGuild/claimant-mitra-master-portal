b50897d7291f3187ac3d740b6f59304be89ae162
mayanktilwankar2355@gmail.com <mayanktilwankar2355@gmail.com>
2025-04-04 17:18:11 +0530

    -ByMayank

---
+++ pages/components/caseStatus/inOmbudsman.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const inOmbudsman = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default inOmbudsman
---
+++ pages/components/caseStatus/rejected.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const rejected = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default rejected
